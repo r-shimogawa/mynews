24a09d8116674edff4109dab6a4305f5c7cf109c
r-shimogawa <shimogawa1992@gmail.com>
2022-11-06 14:18:49 +0000

    first commit

---
+++ app/Models/Profile.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    use HasFactory;
+    // 【PHP/Laravel】09_7
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'name' => 'required',
+        'gender' => 'required',
+        'hobby' => 'required',
+        'introduction' => 'required',
+    );
+    
+}
